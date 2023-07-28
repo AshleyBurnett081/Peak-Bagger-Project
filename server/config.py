1af67ef661676054f09198060877ba155694de6e
Ashley <ashleyburnett081@gmail.com>
2023-07-28 09:00:42 -0600

    updated config.py

--- server/config.py
+++ server/config.py
@@ -1,32 +1,72 @@
+import os
+
+
+# Determine the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
+    # Since SQLAlchemy 1.4.x has removed support for the 'postgres://' URI scheme,
+    # update the URI to the postgres database to use the supported 'postgresql://' scheme
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Logging
+    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+    WTF_CSRF_ENABLED = False
+
+
+
+
 # Standard library imports
 
 # Remote library imports
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+# from flask import Flask
+# from flask_cors import CORS
+# from flask_migrate import Migrate
+# from flask_restful import Api
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
 
-# Local imports
+# # Local imports
 
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "8f42a73054b1749f8f58848be5e6502c"
-app.json.compact = False
+# # Instantiate app, set attributes
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.secret_key = "8f42a73054b1749f8f58848be5e6502c"
+# app.json.compact = False
 
-# Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
-db.init_app(app)
+# # Define metadata, instantiate db
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+# db = SQLAlchemy(metadata=metadata)
+# migrate = Migrate(app, db)
+# db.init_app(app)
 
-# Instantiate REST API
-api = Api(app)
+# # Instantiate REST API
+# api = Api(app)
 
-# Instantiate CORS
-CORS(app)
+# # Instantiate CORS
+# CORS(app)
